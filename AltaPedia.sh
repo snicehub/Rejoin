3bcb794a7b40fdb3b06fc64d686866d70650a7f9
snicehub <snicehere@gmail.com>
2026-08-28 16:08:47 +0700

    Update package search term from 'altapedia' to 'com.altapedia'

--- AltaPedia.sh
+++ AltaPedia.sh
@@ -13,11 +13,11 @@ fi
 run_clear_cache() {
     stty sane 2>/dev/null
     clear
-    echo -e "\033[1;33m[*] Memindai aplikasi yang mengandung kata 'altapedia'...\033[0m"
-    PACKAGES=$($SU_CMD "pm list packages" | sed 's/package://g' | tr -d '\r' | grep 'altapedia' | sort)
+    echo -e "\033[1;33m[*] Memindai aplikasi yang mengandung kata 'com.altapedia'...\033[0m"
+    PACKAGES=$($SU_CMD "pm list packages" | sed 's/package://g' | tr -d '\r' | grep 'com.altapedia' | sort)
 
     if [ -z "$PACKAGES" ]; then
-        echo -e "\n\033[1;31m[!] Tidak ada aplikasi dengan kata 'altapedia' yang terdeteksi! Sistem dibatalkan.\033[0m"
+        echo -e "\n\033[1;31m[!] Tidak ada aplikasi dengan kata 'com.altapedia' yang terdeteksi! Sistem dibatalkan.\033[0m"
         sleep 2
         return
     fi
@@ -43,11 +43,11 @@ run_clear_cache() {
 run_auto_join() {
     stty sane 2>/dev/null
     clear
-    echo -e "\033[1;33m[*] Memindai aplikasi yang mengandung kata 'altapedia'...\033[0m"
+    echo -e "\033[1;33m[*] Memindai aplikasi yang mengandung kata 'com.altapedia'...\033[0m"
     PACKAGES=$($SU_CMD "pm list packages" | sed 's/package://g' | tr -d '\r' | grep 'altapedia' | sort)
 
     if [ -z "$PACKAGES" ]; then
-        echo -e "\n\033[1;31m[!] Tidak ada aplikasi dengan kata 'altapedia' yang terdeteksi! Sistem dibatalkan.\033[0m"
+        echo -e "\n\033[1;31m[!] Tidak ada aplikasi dengan kata 'com.altapedia' yang terdeteksi! Sistem dibatalkan.\033[0m"
         sleep 2
         return
     fi
